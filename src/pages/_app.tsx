9130b93b7ab715fc9152b87e1b0dc50e3a0d5bbe
Ben Mark <bm7382598@gmail.com>
2023-09-10 00:28:04 +0300

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
-import { inter, theme } from '../themes/protheme'
+import { inter, theme } from "@/themes/protheme"
 import {store} from "@/store";
 import { Provider } from "react-redux";
 
-
-// import '../css/example.css'
-// import 'swiper/swiper-bundle.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
