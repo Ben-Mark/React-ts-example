6726ef27c2466c20f653e278f4a3c2292689e4b1
Ben Mark <bm7382598@gmail.com>
2023-09-10 09:27:19 +0300

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,18 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import { AppProps } from 'next/app'
-import { inter, theme } from "@/themes/protheme"
-import {store} from "@/store";
+import { ChakraProvider } from "@chakra-ui/react";
+import { AppProps } from "next/app";
+import { inter, theme } from "@/themes/protheme";
+import { store } from "@/store";
 import { Provider } from "react-redux";
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-      <Provider store={store}>
-    <ChakraProvider theme={theme}>
-      <main className={inter.className}>
-        <Component {...pageProps} />
-      </main>
-    </ChakraProvider>
-      </Provider>
-  )
+    <Provider store={store}>
+      <ChakraProvider theme={theme}>
+        <main className={inter.className}>
+          <Component {...pageProps} />
+        </main>
+      </ChakraProvider>
+    </Provider>
+  );
 }
