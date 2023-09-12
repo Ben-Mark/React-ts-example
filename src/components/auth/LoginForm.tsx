b2025d32c69b97f42486cd31a419bc2fc0c9b45d
Ben Mark <bm7382598@gmail.com>
2023-09-12 07:36:43 +0300

    added playground

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -11,16 +11,24 @@ export const LoginForm = () => {
   const router = useRouter();
 
 
-  const handleClick = (event: any) => {
+  const handleAnimalsClick = (event: any) => {
     event.preventDefault();
     router.push('/animals');
+  };
+
+  const handlePlaygroundClick = (event: any) => {
+    event.preventDefault();
+    router.push('/playground');
   };
 
   return (
         <Stack spacing='5'>
 
-          <Button colorScheme='blue' onClick={(e)=>handleClick(e)}>
+          <Button colorScheme='blue' onClick={(e)=>handleAnimalsClick(e)}>
             Animals
+          </Button>
+          <Button colorScheme='blue' onClick={(e)=>handlePlaygroundClick(e)}>
+            Playground
           </Button>
         </Stack>
   );
