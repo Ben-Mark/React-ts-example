d3f73fc66f2ecce371793b0c0be2326f5ef6acf5
Ben Mark <bm7382598@gmail.com>
2023-09-12 07:44:03 +0300

    added playground

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -3,25 +3,11 @@ import { Flex, Box } from "@chakra-ui/react";
 
 const PlaygroundPage = () => {
   return (
-    <Flex
-      height="100vh"
-      width="100%"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Box
-        width="100px"
-        height="100px"
-        backgroundColor="blue.500"
-        marginRight="5vw">
-      </Box>
-
-      <Box
-        width="100px"
-        height="100px"
-        backgroundColor="blue.500">
-      </Box>
-    </Flex>
+    <Box
+      width="100px"
+      height="100px"
+      backgroundColor="blue.500">
+    </Box>
   );
 };
 
